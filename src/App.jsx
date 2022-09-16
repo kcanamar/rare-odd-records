65ddab215b122e1420a9e612191cb388b6b18d4f
Nathan Yi <nathanyi112@gmail.com>
2022-09-16 15:20:02 -0500

    creates nav bar with css

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
-import About from "./pages/about";
-import Artists from "./pages/artists";
-import Contact from "./pages/contact";
-import Home from "./pages/home";
-import NotFound from "./pages/notFound";
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-export default function App() {
+import About from './pages/about'
+import Artists from './pages/artists'
+import Contact from './pages/contact'
+import Home from './pages/home'
+import NotFound from './pages/notFound'
+import Nav from './components/nav/Nav'
+
+export default function App () {
   return (
-    <div>
-      <About/>
-      <Artists/>
-      <Contact/>
-      <Home/>
-      <NotFound/>
-    </div> 
-  );
+    <BrowserRouter>
+      <Nav />
+      <About />
+      <Artists />
+      <Contact />
+      <Home />
+      <NotFound />
+    </BrowserRouter>
+  )
 }
---
+++ src/styled-components/Layout.jsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+import theme from "../assets/theme";
+
+export const Container = styled.div` 
+    width: ${theme.wlg};
+    max-width: 1920px;
+    margin-inline: auto;
+`
