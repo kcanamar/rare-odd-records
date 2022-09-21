4f3dd15f530f2de5ac6b92b1c7b0de3c410066aa
Kyle Canamar <canamar@Kyles-MacBook-Pro-2.local>
2022-09-21 11:53:48 -0700

    updates

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -16,25 +16,25 @@ export default function Footer() {
                     Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet repellat optio ipsa tenetur odio, harum dicta expedita error laudantium quia.
                     </StyledFooterP>
                     <FooterSocials>
-                        <a href="https://linkedin.com/" target="_blank" rel="noreferer noopener"><FaLinkedin/></a>
-                        <a href="https://faceboook.com/" target="_blank" rel="noreferer noopener"><FaFacebook/></a>
-                        <a href="https://twitter.com/" target="_blank" rel="noreferer noopener"><AiOutlineTwitter/></a>
-                        <a href="https://instagram.com/" target="_blank" rel="noreferer noopener"><AiFillInstagram/></a>
+                        <a href="https://linkedin.com/" target="_blank" rel="noreferrer noopener"><FaLinkedin/></a>
+                        <a href="https://faceboook.com/" target="_blank" rel="noreferrer noopener"><FaFacebook/></a>
+                        <a href="https://twitter.com/" target="_blank" rel="noreferrer noopener"><AiOutlineTwitter/></a>
+                        <a href="https://instagram.com/" target="_blank" rel="noreferrer noopener"><AiFillInstagram/></a>
                     </FooterSocials>
                 </FooterArticle>
                 <FooterArticle className="links">
                     <StyledH4 className="footer-links">Perma Links</StyledH4>
-                    <StyledLink to="/about" >About</StyledLink>
-                    <StyledLink to="/artists" >Artists</StyledLink>
-                    <StyledLink to="/contact" >Contact</StyledLink>
-                    <StyledLink to="/gallery" >Gallery</StyledLink>
+                    <StyledLink to="/about" rel="noreferrer noopener">About</StyledLink>
+                    <StyledLink to="/artists" rel="noreferrer noopener">Artists</StyledLink>
+                    <StyledLink to="/contact" rel="noreferrer noopener">Contact</StyledLink>
+                    <StyledLink to="/gallery" rel="noreferrer noopener">Gallery</StyledLink>
                 </FooterArticle>
                 <FooterArticle className="links">
                     <StyledH4 className="footer-links">Insights</StyledH4>
-                    <StyledLink to="/s" >Blog</StyledLink>
-                    <StyledLink to="/s" >Events</StyledLink>
-                    <StyledLink to="/s" >Communities</StyledLink>
-                    <StyledLink to="/s" >FAQs</StyledLink>
+                    <StyledLink to="/s" rel="noreferrer noopener">Blog</StyledLink>
+                    <StyledLink to="/s" rel="noreferrer noopener">Events</StyledLink>
+                    <StyledLink to="/s" rel="noreferrer noopener">Communities</StyledLink>
+                    <StyledLink to="/s" rel="noreferrer noopener">FAQs</StyledLink>
                 </FooterArticle>
                 <FooterArticle className="links">
                     <StyledH4 className="footer-links">Get In touch</StyledH4>
@@ -43,7 +43,7 @@ export default function Footer() {
                 </FooterArticle>
             </StyledContainer>
             <CopyRight>
-                <small>2022 <a href="https://www.linkedin.com/in/kyle-canamar/" target="_blank">Kcanamar</a> & <a href="https://www.linkedin.com/in/yi-nathan/" target="_blank">Yinathan</a> &copy; All Rights Reserved</small>
+                <small>2022 <a href="https://www.linkedin.com/in/kyle-canamar/" target="_blank" rel="noreferrer noopener">Kcanamar</a> & <a href="https://www.linkedin.com/in/yi-nathan/" target="_blank" rel="noreferrer noopener">Yinathan</a> &copy; All Rights Reserved</small>
             </CopyRight>
         </StyledFooter>
     )
